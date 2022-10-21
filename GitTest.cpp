cae447c2fba6cec82b69d066a4c99ebc6769ae96
k022g1052@g.neec.ac.jp <k022g1052@g.neec.ac.jp>
2022-10-21 09:37:50 +0900

    2行目をnew branch changedに変更

--- GitTest.cpp
+++ GitTest.cpp
@@ -8,7 +8,7 @@ int main()
 
     std::cout << "Hello GitHub World!\n";
 
-    std::cout << "Hello New Branch!\n";
+    std::cout << "New Branch Changed!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
